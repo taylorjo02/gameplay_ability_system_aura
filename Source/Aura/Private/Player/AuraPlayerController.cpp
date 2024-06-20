c08f4be4ed47f433521309e80be0f64be1677ae8
taylorjo02 <joshua.d.taylor122@gmail.com>
2024-06-19 23:39:58 -0400

    add functions for player subsystem configurations and header files

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,37 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// check will assert auracontext pointer can be found, and halt execution if not
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	// init var for input mode to configure below
+	FInputModeGameAndUI InputModeData;
+
+	// makes sure not to lock mouse to the viewport
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+
+	// prevents hiding the cursor once we click into the viewport
+	InputModeData.SetHideCursorDuringCapture(false);
+
+	SetInputMode(InputModeData);
+}
