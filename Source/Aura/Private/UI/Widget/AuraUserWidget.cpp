3db21981330f9b6e7fd2a745e1ab66e8ef745d88
taylorjo02 <joshua.d.taylor122@gmail.com>
2024-07-07 23:41:36 -0400

    Add Widgets for Health & Mana, Create HUD Class

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright DVNT Games
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright DVNT Games
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
