a8f628d7248aaaf74e8506e0cc19323babcd9dfa
taylorjo02 <joshua.d.taylor122@gmail.com>
2024-09-09 23:42:50 -0400

    add broadcast initial values function & setup blueprints for casting initial health values to overlay widget controller

--- Source/Aura/Private/UI/HUD/AuraHUD.cpp
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -32,6 +32,8 @@ void AAuraHUD::InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySyst
 	UOverlayWidgetController* WidgetController = GetOverlayWidgetController(WidgetControllerParams);
 
 	OverlayWidget->SetWidgetController(WidgetController);
+
+	WidgetController->BroadcastInitialValues();
 	
 	Widget->AddToViewport();
 }
